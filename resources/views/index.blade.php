ff4b4cb8436826087915186aa4fb29d5ba902b17
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-02-21 15:09:41 +0100

    creato rotta e controller per pagina principale index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.layout')
+
+@section('segnapostoContent')
+<p>{{ $vivo }}</p>
+@endsection
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="it" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="{{ asset("css/app.css")}}">
+    <title>Laravel-crud by Dario Mantovani</title>
+  </head>
+  <body>
+    @yield('segnapostoContent')
+  </body>
+</html>
