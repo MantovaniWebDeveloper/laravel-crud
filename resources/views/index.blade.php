3b5574f98ab666b720739d6faf52db55c1e9f439
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-02-21 16:17:34 +0100

    inseriti tutti i btn del index ora inzio lo show

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,12 +3,20 @@
 @section('segnapostoContent')
 @include('_partials._header')
 <div class="container">
+  <div class="row">
+    <div class="col-12 pt-4 pb-4">
+      <a href="#"><button class="btn btn-primary">Nuovo pilota</button></a></a>
+    </div>
+  </div>
   <table class="table table-hover">
   <thead class="">
     <tr>
-      <th scope="col">Nome</th>
-      <th scope="col">Cognome</th>
-      <th scope="col">Team</th>
+      <th scope="col text-center">Nome</th>
+      <th scope="col text-center">Cognome</th>
+      <th scope="col text-center">Team</th>
+      <th scope="col text-center">Scheda</th>
+      <th scope="col text-center">Aggiorna</th>
+      <th scope="col text-center">Cancella</th>
     </tr>
   </thead>
   <tbody>
@@ -17,9 +25,11 @@
         <td>{{ $driver["name"]}}</td>
         <td>{{ $driver["surname"]}}</td>
         <td>{{ $driver["team"]}}</td>
+        <td><a href="#"><button class="btn btn-success">Visualizza</button></a></td>
+        <td><a href="#"><button class="btn btn-warning">Aggiorna</button></a></td>
+        <td><a href="#"><button class="btn btn-danger">Cancella</button></a></td>
       </tr>
     @endforeach
-
   </tbody>
 </table>
 </div>
